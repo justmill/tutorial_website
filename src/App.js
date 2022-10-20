00d7e2010d7e6d0366b47614ef42c1a0a735306c
justmill <justmill00@gmail.com>
2022-10-20 18:29:33 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/NavBar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/Pages/Home';
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Navbar />
           <Switch>
             <Route path='/' exact component={Home}/>
+            <Route path='/services' component={Services}/>
+            <Route path='/products' component={Products}/>
+            <Route path='/sign-up' component={SignUp}/>
          </Switch>
       </Router>
       </>
