bc7be13ad736054a45334f47adf05f96cbc37131
justmill <justmill00@gmail.com>
2022-10-23 22:20:21 -0500

    Final Commit

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Pages/Home';
 import Services from './components/Pages/Services';
 import Products from './components/Pages/Products';
 import SignUp from './components/Pages/SignUp';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -19,10 +20,12 @@ function App() {
             <Route path='/products' component={Products}/>
             <Route path='/sign-up' component={SignUp}/>
          </Switch>
+         <Footer />
       </Router>
       </>
     </div>
   );
 }
+
 
 export default App;
